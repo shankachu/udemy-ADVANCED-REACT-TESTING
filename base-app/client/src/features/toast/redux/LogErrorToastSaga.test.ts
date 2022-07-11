e68929fc6088060d037504bf63f0897cdf7547d8
s.cao <shanshan.cao@thalesdigital.io>
2022-07-11 11:43:07 +0200

    second saga test working

--- base-app/client/src/features/toast/redux/LogErrorToastSaga.test.ts
+++ base-app/client/src/features/toast/redux/LogErrorToastSaga.test.ts
@@ -17,9 +17,27 @@ const errorToastAction = {
   payload: errorToastOptions,
 };
 
+const infoToastOptions: ToastOptions = {
+    title: "It gives information",
+    status: "info",
+}
+
+const infoToastAction = {
+    type: "test",
+    payload: infoToastOptions,
+};
+
 test("saga calls analytics when it receives error toast", () => {
   return expectSaga(logErrorToasts, errorToastAction)
       .call(sendToAnalytics, "It's time to panic!!!")
       .run();
 })
+
+test("saga does not call analytics when it receives info toast", () => {
+    return expectSaga(logErrorToasts, infoToastAction)
+        .not.call(sendToAnalytics, "It's not time to panic")
+        .run();
+})
+
+
 
